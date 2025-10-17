d0edd3d4dee459765e0311774bf02c48091de15c
lambdion <70808062+lambdion@users.noreply.github.com>
2025-10-17 18:42:10 +0100

    Use search bar contents if preserved by browser

--- fetch-data.js
+++ fetch-data.js
@@ -3,6 +3,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   const response = await fetch("./webdict.json");
   if (response.ok) {
     dict = await response.json();
-    queryDict(dict, "") // Run first blank query to populate search results with all words
+    queryDict(dict, document.getElementById("search-bar").value) // Run first blank query to populate search results with all words
   }
 });
